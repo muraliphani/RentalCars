7ed6ec4f27d76656da531a445e2b14c2902e003d
Murali Phanikumar Koneti <mukonet1@in.ibm.com>
2022-07-11 12:40:05 +0530

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 
 resource "aws_instance" "web-server-instance"{
-  ami           = "ami-01aab85a5e4a5a0fe"
+  ami           = "ami-085284d24fe829cd0"
   instance_type = "t2.micro"
   key_name      = "project"
   tags = {
